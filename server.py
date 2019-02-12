631719a3d93be97c6ebe5db91cf45a244bf2f9c2
Szanyi Krisztián <szkript@gmail.com>
2019-02-12 14:11:39 +0100

    preparation

--- server.py
+++ server.py
@@ -11,6 +11,22 @@ def hello_world():
     return 'Hello World!'
 
 
+# Todo : route - /question/<question_id>
+
+
+# Todo : route - /add-question
+
+
+# Todo : route - /question/<question_id>
+
+
+# Todo : route - /list?order_by=title &order_direction=desc
+
+
+# Todo : route - /question/<question_id>/edit
+
+
+# Todo : route - /question/<question_id>/delete
 
 
 if __name__ == '__main__':
