af5c0b639d7701b7a376800f2d34c5168cbbaf66
Szanyi Krisztián <szkript@gmail.com>
2019-02-12 13:57:39 +0100

    enesze

--- server.py
+++ server.py
@@ -1,2 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
 # todo: lets seegvcxcbncbn,vc vvb
-svcxbcxvbncvjknjk
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route("/list")
+def hello_world():
+    return 'Hello World!'
+
+
+
+
+if __name__ == '__main__':
+    app.run()
