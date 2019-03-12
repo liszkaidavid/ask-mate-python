3113cc28ed00aba7de07b60f8647aee2cb7d5a83
Napermial <hatvani9823@gmail.com>
2019-03-12 20:30:45 +0100

    loginform dependencies not clear

--- server.py
+++ server.py
@@ -1,9 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
-import data_manager
-import util
+from flask import Flask, render_template, request, redirect, abort, url_for, flash
+import data_manager, util
+from flask_login import LoginManager,login_user
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginManager.LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(LoginManager.user)
+
+        flash('Logged in successfully.')
+
+        next = request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not LoginManager.is_safe_url(next):
+            return abort(400)
+
+        return redirect(next or url_for('index'))
+    return render_template('login.html', form=form)
 
 @app.route('/')
 def home_page():
