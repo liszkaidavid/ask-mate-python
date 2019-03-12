bd175b10d8a4fe954a53652d6194a055de761ce2
Napermial <hatvani9823@gmail.com>
2019-03-12 17:42:06 +0100

    bcrypt hashing/verify

--- util.py
+++ util.py
@@ -1,6 +1,14 @@
-import data_manager
-import bcrypt
+import data_manager, bcrypt
 
 def get_table_titles(table):
     table_titles = [title["column_name"] for title in data_manager.get_title_names(table)]
     return table_titles
+
+def hash_password(plain_text_password):
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
